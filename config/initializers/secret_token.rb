a772edf4ec5d577b75427899811ec0934635e0b2
Cody <cody@vinova.sg>
2011-04-17 20:16:49 +0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoviesPipcody::Application.config.secret_token = '0c5537aa3349a6973b6f167c60a1c0bfeabfcaca287e7ff940fbd0462439728e9ff2f471dbb1583e71ec393311d8496cd951fd69b93b43bd2c73f9ef35ed81a4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MoviesPipcody::Application.config.session_store :cookie_store, :key => '_movies.pipcody_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MoviesPipcody::Application.config.session_store :active_record_store
