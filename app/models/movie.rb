2b84b5ba034cbc997560025b604fdb1e23d62ab4
Cody Nguyen <cody.haind@gmail.com>
2011-04-27 11:08:36 +0700

    annotated Movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+# Schema version: 20110426104017
+#
+# Table name: movies
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  plot       :text
+#  imdb_score :float
+#  runtime    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Movie < ActiveRecord::Base
 end
