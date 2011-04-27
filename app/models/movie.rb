44f37795713ccba2ddf3850fe6d288d8e415923d
Cody Nguyen <cody.haind@gmail.com>
2011-04-27 15:05:37 +0700

    validates presence of name or published name w/ test

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,16 +1,20 @@
 # == Schema Information
-# Schema version: 20110426104017
+# Schema version: 20110427050055
 #
 # Table name: movies
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  plot       :text
-#  imdb_score :float
-#  runtime    :integer
-#  created_at :datetime
-#  updated_at :datetime
+#  id             :integer         not null, primary key
+#  name           :string(255)
+#  plot           :text
+#  imdb_score     :float
+#  runtime        :integer
+#  created_at     :datetime
+#  updated_at     :datetime
+#  published_name :string(255)
 #
 
 class Movie < ActiveRecord::Base
+  
+  validates_presence_of :name, :if => Proc.new { |a| a.published_name.blank? }
+  validates_presence_of :published_name, :if => Proc.new { |a| a.name.blank? }
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,38 @@
 require 'spec_helper'
 
 describe Movie do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = { 
+      :name => "Movie Name",
+      :published_name => "Published Name",
+      :plot => "Movie Plot",
+      :imdb_score => "9.2",
+      :runtime => "142"
+    }
+  end
+  
+  it "should create a new instance given valid attributes" do
+    Movie.create!(@attr)
+  end
+  
+  it "should require a name or a published name" do
+    movie_without_name = Movie.new(@attr.merge(:name => "", 
+                                               :published_name => ""))
+    movie_without_name.should_not be_valid
+  end
+  
+  it "should accept movie with a name but without a published name" do
+    movie_with_name = Movie.new(@attr.merge(:published_name => ""))
+    movie_with_name.should be_valid
+  end
+  
+  it "should accept movie with a published name but without a name" do
+    movie_with_published_name = Movie.new(@attr.merge(:name => ""))
+    movie_with_published_name.should be_valid
+  end
+  
+  it "should accept movie with both names" do
+    movie_with_both_names = Movie.new(@attr)
+    movie_with_both_names.should be_valid
+  end
 end
